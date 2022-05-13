340e2d9d82569eb61fa8bb5ad2043d6c61e9197a
Harsh Sharma <76774500+Harshu1441@users.noreply.github.com>
2022-05-13 11:20:21 -0700

    database-connect updated

--- Database.py
+++ Database.py
@@ -1,11 +1,26 @@
-import mysql.connector
+import mysql.connector as msql
+from mysql.connector import Error
+import csv 
 
-mydb = mysql.connector.connect(
-  host="database-1.catbdoykbe7o.ap-south-1.rds.amazonaws.com",
-  user="",
-  password=""
-)
 
-print(mydb)                        
-                            
-mydb.close()
+
+conn = msql.connect(host='-----', user='-----',  
+                        password='-----')#give ur username, password
+
+
+csv_data = csv.reader(open('csvv.csv'))
+next(csv_data)
+cursor=conn.cursor()
+for row in csv_data:
+    cursor.execute('INSERT INTO ---table-name---(col1,col2,col3) VALUES(%s,%s, %s)',row)
+
+
+conn.commit()
+print("Total number of rows in table: ", cursor.rowcount)
+print(cursor.rowcount, "record inserted.")
+
+result = cursor.fetchall()
+for row in result:
+    print(row)
+    print("\n")
+cursor.close()
