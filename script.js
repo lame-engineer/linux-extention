08b1850b7dc4cbd09978e47bc21135182a577964
Kurumi-01 <100143322+Kurumi-01@users.noreply.github.com>
2022-05-20 19:46:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,7 @@ fetch('')   //Add the url of your API
   .then(data => {
           console.log(data);
           document.getElementById('').  //Add HTML element ID
-          innerHTML=data[0].name;
-          document.getElementById('').  //Add HTML element ID
-          innerHTML=data[0].last;
-
+          innerHTML=data;
         })
 
   .catch(error => {
