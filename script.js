4c5f07170999c4b6ed927123fe18c8931f708758
lame-engineer <88873676+lame-engineer@users.noreply.github.com>
2022-05-27 13:10:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-fetch('')   //Add the url of your API
+fetch('http://ec2-65-2-111-197.ap-south-1.compute.amazonaws.com:8888/')
   .then(response => {console.log(response);
         return response.json()
     })
   .then(data => {
           console.log(data);
-          document.getElementById('').  //Add HTML element ID
-          innerHTML=data;
+          document.getElementById('loading').
+          innerHTML=data[0].name;
+          document.getElementById('Linux').
+          innerHTML=data[0].last;
+
         })
 
   .catch(error => {
       console.log("error!")
       console.error(error)
   })
-    
